d5b623fc6189d487f31490d47414b71b5485e737
garviin <garvibulkin@gmail.com>
2019-09-18 22:17:22 +1000

    created component for testing file uploads

---
+++ client/src/components/ImageUpload.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import {
+    Button,
+    Form,
+} from "semantic-ui-react";
+
+export default class ImageUpload extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            file: null
+        };
+    }
+
+    fileInputRef = React.createRef();
+
+    fileChange = e => {
+        this.setState({ file: e.target.files[0] }, () => {
+          console.log("File chosen --->", this.state.file);
+        });
+    };
+
+    render() {
+        return (
+            <Form.Field>
+                <Button
+                  content="Choose File"
+                  labelPosition="left"
+                  icon="file"
+                  onClick={() => this.fileInputRef.current.click()}
+                />
+                <input
+                  ref={this.fileInputRef}
+                  type="file"
+                  hidden
+                  onChange={this.fileChange}
+                />
+              </Form.Field>
+        )
+    }
+}
