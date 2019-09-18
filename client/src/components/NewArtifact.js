4c7888043ca77e68443264ad1a9a965c979bb3d4
Yusuke <npigrandy@gmail.com>
2019-09-18 19:20:59 +1000

    Register artifacts_v2:Able to insert some forms into database through POST

--- client/src/components/NewArtifact.js
+++ client/src/components/NewArtifact.js
@@ -1,109 +1,187 @@
-import React from 'react'
-import { Button, Form, Container, Input, Popup, Icon, Header, Modal, Image, Label } from 'semantic-ui-react'
+import React, {Component} from 'react'
+import { Redirect } from 'react-router';
+import { Button,
+         Form,
+         Container,
+         Input,
+         Popup,
+         Icon,
+         Header,
+         Modal,
+         Image,
+         Label,
+         Message } from 'semantic-ui-react'
 
-const NewArtifact= () => (
-<Form>
-  <Container textAlign='center'>
-       <Form >
+class NewArtifact extends Component {
 
-         {/*Form for name*/}
-         <Form.Group widths='equal'>
-           <Form.Field required>
-            <label> Name
-            <Popup content='This field is mandatory' trigger={
-            <Icon name='info circle' size ='large'/>}/> :
-            </label>
-            <Input placeholder='Input name of artifact'/>
-           </Form.Field>
-           <Form.Field>
-            <label> GeoTag
-            <Popup content='This field is optional' trigger={
-            <Icon name='info circle' size ='large'/>}/> :
-            </label>
-            <Input placeholder='Current location of artifacts'/>
-           </Form.Field>
-         </Form.Group>
+  constructor() {
+    super();
 
-         {/*Form for dates*/}
-         <Form.Group widths='equal'>
-          <Form.Input fluid label='Day:' placeholder='Day'/>
-          <Form.Input fluid label='Month:' placeholder='Month'/>
-          <Form.Input fluid label='Year:' placeholder='Year'/>
-         </Form.Group>
+    this.state = {
+      name: '',
+      geoTag: '',
+      day: '',
+      month: '',
+      year: '',
+      history: '',
+      fireRedirect: false
+      };
 
-         {/*Form for History input*/}
-         <Form.Field>
-          <label> History
-           <Popup content=
-           'This field is optional. If possible, please specify a brief history of the artifacts'trigger={
-           <Icon name='info circle' size ='large'/>}/> :
-          </label>
-          <Form.TextArea placeholder='A short description'/>
-         </Form.Field>
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-         {/*Tags for artifacts*/}
-         <Form.Field>
-           <label> Tags
-            <Popup content=
-            'This field is optional. Able to support multiple tags!'trigger={
-            <Icon name='info circle' size ='large'/>}/> :
-           </label>
-           <Input
-            icon='tags'
-            iconPosition='left'
-            label={{ tag: true, content: 'Add Tag' }}
-            labelPosition='right'
-            placeholder='Enter tags'
-           />
-         </Form.Field>
+handleSubmit(event) {
+  event.preventDefault();
 
-         {/*On update tag - Still not implemented*/}
-         <div>
-          <Label>
-            Old piano
-            <Icon name='delete' />
-          </Label>
-          <Label>
-            Antique
-            <Icon name='delete' />
-          </Label>
-          <Label>
-            Family treasure
-            <Icon name='delete' />
-          </Label>
-        </div>
+  var data =
+    {
+      name:this.state.name,
+      geoTag:this.state.geoTag,
+      day:this.state.day,
+      month:this.state.month,
+      year:this.state.year,
+      history:this.state.history
+    };
 
-         {/*Modal to upload artifact image*/}
-          <Form.Field>
-            <label> Click this to upload image to cloud
-            <Popup content=
-            'This field is optional.'trigger={
-            <Icon name='info circle' size ='large'/>}/> :
-            </label>
-             <Modal closeIcon trigger={<Button icon='cloud upload'></Button>}>
-              <Modal.Header>Select an Image</Modal.Header>
-              <Modal.Content image>
-                <Image wrapped size='medium'
-                 src='https://www.musicjunction.com.au/wp-content/uploads/2019/03/427DE39AADE447B5A30422DF725647A8_12073_2139x2001_c587b9fef86903b89a823353fa512cf0.jpg' />
-                <Modal.Description textAlign='center'>
-                  <Header textAlign='center'>This image will be uploaded to cloud</Header>
-                  <p>
-                   Please double check with the image selected on the left.
-                   If the selected image is correct, click on the button below
-                   to upload this image to cloud server.
-                  </p>
-                  <p>Is it okay to use this photo?</p>
-                  <Button color='blue' type='submit'>Upload</Button>
-                </Modal.Description>
-              </Modal.Content>
-            </Modal>
-          </Form.Field>
+  fetch('/artifacts/new', { method: 'POST',
 
-         <Button color='primary' type='submit'>Submit</Button>
-       </Form>
-  </Container>
-</Form>
+  body: JSON.stringify(data), // data can be `string` or {object}!
 
-)
+  headers:{ 'Content-Type': 'application/json' } })
+
+  .then(res => res.json())
+
+  .catch(error => console.error('Error:', error))
+
+  .then(response => console.log('Successfully added'));
+
+}
+
+   handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Container textAlign='center'>
+          <Form onSubmit={this.handleSubmit}>
+
+            <Form.Group widths='equal'>
+
+              {/*Form for name*/}
+              <Form.Field required>
+               <label> Name
+                 <Popup content='This field is mandatory' trigger={
+                 <Icon name='info circle' size ='large'/>}/> :
+               </label>
+               <Input placeholder='Input name of artifact'
+                name='name' onChange={this.handleChange} />
+              </Form.Field>
+
+              {/*Form for location*/}
+              <Form.Field>
+               <label> GeoTag
+                 <Popup content='This field is optional' trigger={
+                 <Icon name='info circle' size ='large'/>}/> :
+               </label>
+               <Input placeholder='Current location of artifacts'
+                name='geoTag' onChange={this.handleChange}/>
+              </Form.Field>
+            </Form.Group>
+
+            {/*Form for dates*/}
+            <Form.Group widths='equal'>
+              <Form.Input fluid label='Day:' placeholder='Day'
+               name='day' onChange={this.handleChange}/>
+              <Form.Input fluid label='Month:' placeholder='Month'
+               name='month' onChange={this.handleChange}/>
+              <Form.Input fluid label='Year:' placeholder='Year'
+               name='year' onChange={this.handleChange}/>
+            </Form.Group>
+
+            {/*Form for History input*/}
+            <Form.Field>
+              <label> History
+                <Popup content=
+                'This field is optional. If possible, please specify a brief history of the artifacts'trigger={
+                <Icon name='info circle' size ='large'/>}/> :
+              </label>
+
+              <Form.TextArea placeholder='A short description'
+               name='history' onChange={this.handleChange}/>
+            </Form.Field>
+
+            {/*Tags for artifacts*/}
+            <Form.Field>
+              <label> Tags
+                <Popup content=
+                'This field is optional. Able to support multiple tags!'trigger={
+                <Icon name='info circle' size ='large'/>}/> :
+              </label>
+              <Input
+              icon='tags'
+              iconPosition='left'
+              label={{ tag: true, content: 'Add Tag' }}
+              labelPosition='right'
+              placeholder='Disabled' disabled
+              />
+            </Form.Field>
+
+            {/*On update tag - Still not implemented*/}
+            <div>
+              <Label>
+                Old piano
+                <Icon name='delete' />
+              </Label>
+              <Label>
+                Antique
+                <Icon name='delete' />
+              </Label>
+              <Label>
+                Family treasure
+                <Icon name='delete' />
+              </Label>
+            </div>
+
+            {/*Modal to upload artifact image*/}
+            <Form.Field>
+              <label> Click this to upload image to cloud
+                <Popup content=
+                'This field is optional.'trigger={
+                <Icon name='info circle' size ='large'/>}/> :
+              </label>
+              <Modal closeIcon trigger={<Button icon='cloud upload'></Button>}>
+                <Modal.Header>Select an Image</Modal.Header>
+                <Modal.Content image>
+                  <Image wrapped size='medium'
+                  src='https://www.musicjunction.com.au/wp-content/uploads/2019/03/427DE39AADE447B5A30422DF725647A8_12073_2139x2001_c587b9fef86903b89a823353fa512cf0.jpg' />
+                  <Container textAlign='center'>
+                    <Modal.Description textalign='center'>
+                      <Header>This image will be uploaded to cloud</Header>
+                      <p>
+                      Please double check with the image selected on the left.
+                      If the selected image is correct, click on the button
+                      below to upload this image to cloud server.
+                      </p>
+                      <p>Is it okay to use this photo?</p>
+
+                      <Button color='blue' type='submit'>Upload</Button>
+                    </Modal.Description>
+                  </Container>
+                </Modal.Content>
+              </Modal>
+            </Form.Field>
+
+            <Button color='facebook' type='submit'>Submit</Button>
+
+          </Form>
+        </Container>
+      </div>
+    );
+  }
+}
 
 export default NewArtifact
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 require("./services/passport");
-
+const cors = require('cors');
 const app = express();
 
 const port = 5000;
@@ -9,7 +9,7 @@ var db = require('./db/db');
 var bodyParser = require('body-parser')
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
-
+app.use(cors());
 
 require("./routes/authRoutes")(app);
 // require("./routes/artifactRoutes")(app);
@@ -24,7 +24,9 @@ app.get("/members", (req, res) => {
 });
 
 app.get('/api/artifacts', (req,res) =>{
-  db.query('SELECT * FROM artifact', (err, rows, fields) => {
+  //IMPT: Garvin artifact db query was SELECT * FROM artifact
+  //Case sensitive for tables -From Randy
+  db.query('SELECT * FROM Artifact', (err, rows, fields) => {
     if (!err){
       res.json(rows)
     } else {
@@ -47,7 +49,7 @@ function newArtifactID(callback) {
   var high = 10000,
   low = 0;
   return Math.random() * (high - low) + low
-  
+
   // let newID = db.query('select max(artifactID) as maximum from artifact', (err, rows, fields) => {
   //   return callback(rows[0].maximum + 1);
   // });
@@ -61,27 +63,51 @@ function doubleBackticks(inputstr){
     .join('')
 }
 
-app.post('/new/artifact', function(req, res) {
-  var artifactID = newArtifactID(),
-  artifactName = req.body.artifactName,
-  year = req.body.year,
-  description = doubleBackticks(req.body.desc)
-  userID = 1,
-  insertStatement = "INSERT INTO `mydb`.`Artifact` (`ArtifactID`, `Name`, `Geotag`, `Tags`, `DateAddedYear`, `DateAddedMonth`, `DateAddedDay`, `DateSentYear`, `DateSentMonth`, `DateSentDay`, `DateAcquireYear`, `DateAcquireMonth`, `DateAcquireDay`, `AccuracyAdded`, `AccuracyAcquire`, `AccuracySent`, `Text`, `Heir`, `CurrentOwner`, `User_UserID`, `Photo_PhotoID`) VALUES (" + artifactID + ", '" + artifactName + "', NULL, NULL," + year + ", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,'"+ description +"', NULL, NULL," + userID + ", NULL);";
-  console.log(description)
-  db.query(insertStatement,(err, rows, fields) => {
-      if (!err){
-        console.log("Sucess!");
-        res.status(201).end("Success!");
-      } else {
-        console.log(err);
-        res.status(400).end("Failed :(")
-      }
+//POST route for registration of new artifacts
+app.post('/artifacts/new', function(req, res) {
+    const name = req.body.name;
+    const geoTag = req.body.geoTag;
+    const day = req.body.day;
+    const month = req.body.month;
+    const year = req.body.year;
+    const history = req.body.history;
+
+    var artifactID = newArtifactID();
+    var currOwn = 1;
+
+    db.query(`INSERT INTO Artifact SET ArtifactID = ?, Name = ?, Geotag = ?,
+      DateAcquireYear = ?, DateAcquireMonth = ?, DateAcquireDay = ?,
+      description = ?, CurrentOwner = ? `,
+      [artifactID, name, geoTag, year, month, day, history, currOwn], function(err, result) {
+        if (!err) {
+          console.log("Added successfully");
+        } else {
+          console.log(err);
+          res.sendStatus(404);
+        }
     });
 });
+// Previous route /new/artifact
+// app.post('/artifacts/new', function(req, res) {
+//   var artifactID = newArtifactID(),
+//   artifactName = req.body.artifactName,
+//   year = req.body.year,
+//   description = doubleBackticks(req.body.desc)
+//   userID = 1,
+//   insertStatement = "INSERT INTO `mydb`.`Artifact` (`ArtifactID`, `Name`, `Geotag`, `Tags`, `DateAddedYear`, `DateAddedMonth`, `DateAddedDay`, `DateSentYear`, `DateSentMonth`, `DateSentDay`, `DateAcquireYear`, `DateAcquireMonth`, `DateAcquireDay`, `AccuracyAdded`, `AccuracyAcquire`, `AccuracySent`, `Text`, `Heir`, `CurrentOwner`, `User_UserID`, `Photo_PhotoID`) VALUES (" + artifactID + ", '" + artifactName + "', NULL, NULL," + year + ", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,'"+ description +"', NULL, NULL," + userID + ", NULL);";
+//   console.log(description)
+//   db.query(insertStatement,(err, rows, fields) => {
+//       if (!err){
+//         console.log("Sucess!");
+//         res.status(201).end("Success!");
+//       } else {
+//         console.log(err);
+//         res.status(400).end("Failed :(")
+//       }
+//     });
+// });
 
 
 
 
 app.listen(port, () => console.log(`server started on port ${port}`));
-
