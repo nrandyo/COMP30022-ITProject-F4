987417063b69f30a2c73607afac9cc2e1d0618ec
Sean <seanwong1996@gmail.com>
2019-09-21 01:54:38 +1000

    Added date range picker in timeline page, Reformatted frontend code

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,49 +1,49 @@
-import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
-import Navbar from './Navbar';
-import Footer from './Footer';
-import NewArtifact from './NewArtifact';
-import Artifact from './Artifact'
-import Objects from './Objects'
-import Photos from './Photos'
-import Letters from './Letters'
-import ArtifactPage from './ArtifactPage'
-import Timelines from './Timeline'
-import ArtifactNav from './ArtifactNav'
-import Homepage from './Homepage'
-import ThemeTest from './ThemeTest'
-
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import NewArtifact from "./NewArtifact";
+import Artifact from "./Artifact";
+import Objects from "./Objects";
+import Photos from "./Photos";
+import Letters from "./Letters";
+import ArtifactPage from "./ArtifactPage";
+import Timelines from "./Timeline";
+import TimelineNav from "./TimelineNav";
+import ArtifactNav from "./ArtifactNav";
+import Homepage from "./Homepage";
+import ThemeTest from "./ThemeTest";
 
 const App = () => {
-    return (
+  return (
+    <div>
+      {/* Only one child in a router */}
+      <BrowserRouter>
         <div>
-            {/* Only one child in a router */}
-            <BrowserRouter>
-                <div>
-                    {/* Use exact keyword to provide strict path*/}
-                    <Navbar />
-                    
-                    <Route exact path = "/theme" component={ThemeTest}/>
-                    <Route exact path = "/" component={Homepage}/>
-                    <Route exact path = "/timeline" component={Timelines}/>
-                    <Route path = "/artifacts" component = {ArtifactNav}/>
-                    <Route exact path = "/artifacts" component = {Artifact}/>
-                    <Route exact path = "/artifacts/photos" component = {Photos}/>
-                    <Route exact path = "/artifacts/letters" component = {Letters}/>
-                    <Route exact path = "/artifacts/objects" component = {Objects}/>
-                    <Route exact path = "/artifacts/artifactpage" component = {ArtifactPage}/>
-                    <Route path = "/artifacts/new" component = {NewArtifact}/>
-                    <Footer />
-                </div>
-            </BrowserRouter>
+          {/* Use exact keyword to provide strict path*/}
+          <Navbar />
+
+          <Route exact path="/theme" component={ThemeTest} />
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/timeline" component={TimelineNav} />
+          <Route exact path="/timeline" component={Timelines} />
+          <Route path="/artifacts" component={ArtifactNav} />
+          <Route exact path="/artifacts" component={Artifact} />
+          <Route exact path="/artifacts/photos" component={Photos} />
+          <Route exact path="/artifacts/letters" component={Letters} />
+          <Route exact path="/artifacts/objects" component={Objects} />
+          <Route
+            exact
+            path="/artifacts/artifactpage"
+            component={ArtifactPage}
+          />
+          <Route path="/artifacts/new" component={NewArtifact} />
+          <Footer />
         </div>
-    );
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
-
-
-
-
-
--- client/src/components/Timeline.js
+++ client/src/components/Timeline.js
@@ -1,161 +1,101 @@
-import React, { Component } from 'react';
-import {Link} from 'react-router-dom'
-import { Input, Menu, Button, Icon, Image, Item, Label, Container, Header, Segment } from 'semantic-ui-react'
-import Timeline from 'react-timeline-semantic-ui'
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
-import { MdAddAPhoto, MdPhoto, MdMail } from "react-icons/md";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import {
+  Input,
+  Menu,
+  Button,
+  Icon,
+  Image,
+  Item,
+  Label,
+  Container,
+  Header,
+  Segment
+} from "semantic-ui-react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { MdPhoto, MdMail } from "react-icons/md";
 
 const Timelines = () => {
+  return (
+    <Container>
+      <VerticalTimeline>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2011 - present"
+          iconStyle={{ background: "#00617F", color: "#fff" }}
+          icon={<MdMail />}
+        >
+          <h3 className="vertical-timeline-element-title">Event 1</h3>
+          <h4 className="vertical-timeline-element-subtitle">Location 1</h4>
+          <p>Description</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2010 - 2011"
+          iconStyle={{ background: "#8fb4c4", color: "#fff" }}
+          icon={<MdPhoto />}
+        >
+          <h3 className="vertical-timeline-element-title">Event 2</h3>
+          <h4 className="vertical-timeline-element-subtitle">Location 2</h4>
+          <p>Significant event</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2008 - 2010"
+          iconStyle={{ background: "#00617F", color: "#fff" }}
+          icon={<MdMail />}
+        >
+          <h3 className="vertical-timeline-element-title">Event 3</h3>
+          <h4 className="vertical-timeline-element-subtitle">Location 3</h4>
+          <p>Description</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2006 - 2008"
+          iconStyle={{ background: "#8fb4c4", color: "#fff" }}
+          icon={<MdPhoto />}
+        >
+          <h3 className="vertical-timeline-element-title">Event 4</h3>
+          <h4 className="vertical-timeline-element-subtitle">Location 4</h4>
+          <p>Description</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="April 2013"
+          iconStyle={{ background: "#00617F", color: "#fff" }}
+          icon={<MdMail />}
+        >
+          <h3 className="vertical-timeline-element-title">Event 5</h3>
+          <h4 className="vertical-timeline-element-subtitle">Location 5</h4>
+          <p>Description</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="November 2012"
+          iconStyle={{ background: "#8fb4c4", color: "#fff" }}
+          icon={<MdPhoto />}
+        >
+          <h3 className="vertical-timeline-element-title">Event 6</h3>
+          <h4 className="vertical-timeline-element-subtitle">Location 6</h4>
+          <p>Description</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2002 - 2006"
+          iconStyle={{ background: "#00617F", color: "#fff" }}
+          icon={<MdMail />}
+        >
+          <h3 className="vertical-timeline-element-title">Event 7</h3>
+          <h4 className="vertical-timeline-element-subtitle">Location 7</h4>
+          <p>Description</p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </Container>
+  );
+};
 
-    return(
-        <Container >
-          <Header
-            as='h2'
-            textAlign='center'
-            content='Timeline'
-            subheader='A timeline of significant events'
-          />
-          <VerticalTimeline>
-            <VerticalTimelineElement
-              className="vertical-timeline-element--work"
-              date="2011 - present"
-              iconStyle={{ background: '#00617F', color: '#fff' }}
-              icon=    {<MdMail/>}
-            >
-              <h3 className="vertical-timeline-element-title">Event 1</h3>
-              <h4 className="vertical-timeline-element-subtitle">Location 1</h4>
-              <p>
-                Description
-              </p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-              className="vertical-timeline-element--work"
-              date="2010 - 2011"
-              iconStyle={{ background: '#8fb4c4', color: '#fff' }}
-              icon={<MdPhoto/>}
-            >
-              <h3 className="vertical-timeline-element-title">Event 2</h3>
-              <h4 className="vertical-timeline-element-subtitle">Location 2</h4>
-              <p>
-                Significant event
-              </p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-              className="vertical-timeline-element--work"
-              date="2008 - 2010"
-              iconStyle={{ background: '#00617F', color: '#fff' }}
-              icon={<MdMail/>}
-            >
-              <h3 className="vertical-timeline-element-title">Event 3</h3>
-              <h4 className="vertical-timeline-element-subtitle">Location 3</h4>
-              <p>
-                Description
-              </p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-              className="vertical-timeline-element--work"
-              date="2006 - 2008"
-              iconStyle={{ background: '#8fb4c4', color: '#fff' }}
-              icon={<MdPhoto/>}
-            >
-              <h3 className="vertical-timeline-element-title">Event 4</h3>
-              <h4 className="vertical-timeline-element-subtitle">Location 4</h4>
-              <p>
-                Description
-              </p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-              className="vertical-timeline-element--education"
-              date="April 2013"
-              iconStyle={{ background: '#00617F', color: '#fff' }}
-              icon={<MdMail/>}
-            >
-              <h3 className="vertical-timeline-element-title">Event 5</h3>
-              <h4 className="vertical-timeline-element-subtitle">Location 5</h4>
-              <p>
-                Description
-              </p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-              className="vertical-timeline-element--education"
-              date="November 2012"
-              iconStyle={{ background: '#8fb4c4', color: '#fff' }}
-              icon={<MdPhoto/>}
-            >
-              <h3 className="vertical-timeline-element-title">Event 6</h3>
-              <h4 className="vertical-timeline-element-subtitle">Location 6</h4>
-              <p>
-                Description
-              </p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-              className="vertical-timeline-element--education"
-              date="2002 - 2006"
-              iconStyle={{ background: '#00617F', color: '#fff' }}
-              icon={<MdMail/>}
-            >
-              <h3 className="vertical-timeline-element-title">Event 7</h3>
-              <h4 className="vertical-timeline-element-subtitle">Location 7</h4>
-              <p>
-                Description
-                </p>
-            </VerticalTimelineElement>
-          </VerticalTimeline>
-          {/* <Timeline
-            direction="right"
-            icon="uers"
-            title="Event 1"
-            time="Date"
-            description="Description."
-            color='blue'
-            tags={['tag1', 'tag2']}
-            lineHeight={3}
-          />
-          <Timeline 
-            direction="left"
-            icon="user"
-            title="Event 2"
-            time="Date"
-            description="Description."
-            color='grey'
-            tags={['tag1', 'tag2']}
-            lineHeight={3}
-          />
-          <Timeline
-            direction="right"
-            icon="user"
-            title="Event 3"
-            time="Date"
-            description="Description."
-            color='blue'
-            tags={['tag1', 'tag2']}
-            lineHeight={3}
-          />
-          <Timeline 
-            direction="left"
-            icon="user"
-            title="Event 4"
-            time="Date"
-            description="Description."
-            color='grey'
-            tags={['tag1', 'tag2']}
-            lineHeight={3}
-          />
-          <Timeline
-            direction="right"
-            icon="user"
-            title="Event 5"
-            time="Date"
-            description="Description."
-            color='blue'
-            tags={['tag1', 'tag2']}
-            lineHeight={3}
-          /> */}
-        </Container>
-    )
-    
-}
-
-
-export default Timelines
+export default Timelines;
