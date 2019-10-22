e9f94b910753a53cacfdf97aaa99a572e269d8b9
garviin <garvibulkin@gmail.com>
2019-10-22 20:16:17 +1100

    added heroku procfile

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("./services/passport");
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 var db = require("./db/db");
 var bodyParser = require("body-parser");
