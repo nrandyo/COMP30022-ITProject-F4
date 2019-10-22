160545053f177d97b57bff8a47ee1f595cb54bb6
garviin <garvibulkin@gmail.com>
2019-10-22 20:41:55 +1100

    heroku v.1.2

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("./services/passport");
 const app = express();
+const path = require('path')
 
 const port = process.env.PORT || 5000;
 
@@ -21,5 +22,13 @@ app.get("/members", (req, res) => {
     else console.log(err);
   });
 });
+
+if (process.env.NODE_ENV === "production"){
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
+}
 
 app.listen(port, () => console.log(`server started on port ${port}`));
